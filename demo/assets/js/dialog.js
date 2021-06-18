507d0a81f3182fa97f86ceba4e2573176e2bb10c
leewenyu1023 <79963926+leewenyu1023@users.noreply.github.com>
2021-06-18 18:11:31 +0800

    Add files via upload

---
+++ demo/assets/js/dialog.js
@@ -0,0 +1,24 @@
+
+		/*彈跳視窗*/
+		var dialog,dialog1;
+		window.onload=function(){
+			dialog=document.getElementById("dialog");
+		};
+		function showDialog(){
+			dialog.style.display="block";
+		};
+		function closeDialog(){
+			dialog.style.display="none";
+		};
+		function showSearch(){
+			search.style.display="block";
+		};
+		function closeSearch(){
+			search.style.display="none";
+		};
+		function showMember(){
+			member.style.display="block";
+		};
+		function closeMember(){
+			member.style.display="none";
+		};
